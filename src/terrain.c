2f7bf0ab395b9fade5095dfd06809219a01be59a
ellarekow <ellarekow@gmail.com>
2022-01-31 21:13:57 -0600

    Generates different terrain elements

--- src/terrain.c
+++ src/terrain.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define ROWS 21
 #define COLUMNS 80
+#define SEED 32
 
 char BLOCK[ROWS][COLUMNS];
 
@@ -23,25 +25,62 @@ void edge_gen(){
 void path_gen(){
     // N to S path
     int row;
-    int column = rand() % COLUMNS;
+    int column = rand() % (COLUMNS - 2) + 1;
 
     for(row = 0; row < ROWS; row++){
+        srand(time(NULL));
         BLOCK[row][column] = '#';
+        row += (rand() % 2) - 1;
     }
 
-    row = rand() % (ROWS - 2) + 1;
+    srand(SEED);
+    row = rand() % (ROWS - 3) + 1;
 
     for(column = 0; column < COLUMNS; column++){
+        srand(time(NULL));
         BLOCK[row][column] = '#';
+        column += (rand() % 2) - 1;
     }
+    srand(SEED);
 }
 
-int main(){
-    srand(32);
+void tall_gen(int size, int startR, int startC){
+    int row; 
+    int column; 
 
-    printf("\n");
+    for(row = startR; row < size; row++){
+        for(column = startC; column < size; column++){
+            BLOCK[row][column] = ',';
+        }
+    }
+    
+}
+
+void clear_gen(int size, int startR, int startC){
+    int row; 
+    int column; 
+
+    for(row = startR; row < size; row++){
+        for(column = startC; column < size; column++){
+            BLOCK[row][column] = '.';
+        }
+    }
+    
+}
+
+void gen_terrain(){
     edge_gen();
     path_gen();
+    // tall_gen(7, 1, 1);
+    clear_gen(7, 1, 1);
+}
+
+
+int main(){
+    srand(SEED);
+
+    printf("\n");
+    gen_terrain();
 
     int row, column;
 
