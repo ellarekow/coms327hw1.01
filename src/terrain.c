e0c86f8e30dea3e53e5ab2354af1b7124289aec5
ellarekow <ellarekow@gmail.com>
2022-02-01 19:38:25 -0600

    Edits to generations stuff

--- src/terrain.c
+++ src/terrain.c
@@ -28,7 +28,13 @@ void path_gen(){
 
     int random = rand() % 12;
 
+    
     for(row = 0; row < ROWS; row++){
+        if(column == 0)
+            column++;
+        else if(column == ROWS - 1)
+            column--;
+
         BLOCK[row][column] = '#';
         if(random % 3 == 0 && column < COLUMNS - 1)
             column += (rand() % 3) - 1;
@@ -39,6 +45,11 @@ void path_gen(){
     row = rand() % (ROWS - 3) + 1;
 
     for(column = 0; column < COLUMNS; column++){
+        if(row == 0)
+            row++;
+        else if(row == ROWS - 1)
+            row--;
+
         BLOCK[row][column] = '#';
         if(random % 3 == 0 && row < ROWS - 1)
             row += (rand() % 3) - 1;
@@ -47,9 +58,12 @@ void path_gen(){
     }
 }
 
-void tall_gen(int size, int startR, int startC){
+void patch_gen(int size, int startR, int startC, char c){
     int row; 
     int column; 
+
+    int sizeR, sizeC;
+    
 
     if(startR == 0)
         startR += 1;
@@ -57,72 +71,67 @@ void tall_gen(int size, int startR, int startC){
     if(startC == 0)
         startC += 1;
 
-    for(row = startR; row < (size + row) && (row < ROWS - 1); row++){
-        for(column = startC; column < (size + column) && (column < COLUMNS - 1); column++){
+    sizeR = size;
+    for(row = startR; size >= 0 && row < ROWS - 1; row++){
+        sizeC = sizeR;
+        for(column = startC; size >= 0 && column < COLUMNS - 1; column++){
             if(BLOCK[row][column] == ' ')
-                BLOCK[row][column] = ',';
+                BLOCK[row][column] = c;
+            sizeC--;
         }
+        sizeR--;
+
     }
     
 }
 
-void clearing_gen(int size, int startR, int startC){
-    int row; 
-    int column; 
-
-    if(startR == 0)
-        startR += 1;
-
-    if(startC == 0)
-        startC += 1;
-
-    for(row = startR; row < (size + row) && (row < ROWS - 1); row++){
-        for(column = startC; column < (size + column) && (column < COLUMNS - 1); column++){
-            if(BLOCK[row][column] == ' ')
-                BLOCK[row][column] = '.';
-        }
-    }
+void mart_gen(){
     
 }
 
 void gen_terrain(){
     edge_gen();
-
-    int size = (rand() % 20) + 2;
-    int row = (rand() % 20) + 2;
-    int column = (rand() % 20) + 2; 
-
+    
+    int size = (rand() % 3);
+    int row = (rand() % 5) + 2;
+    int column = (rand() % 10) + 2; 
     int number = (rand() % 10) + 2;
 
-    int what = (rand() % 100) + 1;
 
-    int i; 
+    //loop to generate ~2 clearing and ~2 long grass
+    int i;
+    for(i = 0; i < 4; i++){
+        if(i == 1 || i == 3)
+            patch_gen(size, row, column, '.');
+        else   
+            patch_gen(size, row, column, ',');
+
+        size = (rand() % 3);
+        row = (rand() % 3) + 1;
+        column = (rand() % 5) + 1; 
+    }
+ 
+    int what = rand() % 3;
+
+    //generates other objects
     for(i = 0; i < number; i++){
         
-        if(what % 7 == 2){
+        if(what == 0){
             if(row > 0 && column > 0)
                 BLOCK[row][column] = '%';
         }
-        else if(what % 10 == 3)
+        else if(what == 1)
         {
             if(row > 0 && column > 0)
                 BLOCK[row][column] = '"';
         }
 
-        else if(what % 3 == 1)
-            clearing_gen(size, row, column);
-
-        else if(what % 2 == 0)
-            tall_gen(size, row, column);
-
-
-        size = (rand() % 10) + 2;
+        size = (rand() % 3);
         row = (rand() % ROWS - 2) + 1;
         column = (rand() % COLUMNS - 2) + 1;
-        what = rand() % 5;
+        what = (rand() % 3);
     }
     path_gen();
-
 }
 
 
@@ -139,29 +148,29 @@ int main(){
     char pos; 
     for(row = 0; row < ROWS; row++){
         for(column = 0; column < COLUMNS; column++){
+            // if(BLOCK[row][column] == ' ')
+            //     BLOCK[row][column] = '.';
+
             pos = BLOCK[row][column];
 
-            if(pos == 'C')
-                // red
+            // curses library for color
+
+            if(pos == 'C') // red
                 printf("\033[1;31m");
-            else if(pos == '.')
-                // green
+            else if(pos == '.') // green
                 printf("\033[0;32m");
-            else if(pos == 'M')
-                // blue
+            else if(pos == 'M') // blue
                 printf("\033[0;34m");
-            else if(pos == '#')
-                // white
+            else if(pos == '#') // white
                 printf("\033[0;37m");
-            else if(pos == '"')
-                //yellow 
+            else if(pos == '"') // yellow 
                 printf("\033[0;33m");
-            else if(pos == '%')
-                //black
-                printf("\033[0;30m");
-            else if(pos == ',')
-                //bright green
+            else if(pos == '%') // black
+                printf("\e[0;34m");
+            else if(pos == ',') // bright green
                 printf("\033[92;102m");
+            else
+                printf("\e[0m");
 
             printf("%c", pos);
         }
